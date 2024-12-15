60be868e7a082b6501a35723aba023279969361f
Heather Piwowar <hpiwowar@gmail.com>
2024-12-15 15:09:46 -0800

    include some swipe cards even though it is weird and not currently useful

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,126 @@
-import 'dart:developer';
 import 'dart:convert';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
-import 'package:libsql_dart/libsql_dart.dart';
+import 'package:flutter_card_swiper/flutter_card_swiper.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:basic_utils/basic_utils.dart';
+import 'package:libsql_dart/libsql_dart.dart';
 import 'package:searchable_listview/searchable_listview.dart';
+
+class ExampleCandidateModel {
+  final String name;
+  final String job;
+  final String city;
+  final List<Color> color;
+
+  ExampleCandidateModel({
+    required this.name,
+    required this.job,
+    required this.city,
+    required this.color,
+  });
+}
+
+final List<ExampleCandidateModel> candidates = [
+  ExampleCandidateModel(
+    name: 'One, 1',
+    job: 'Developer',
+    city: 'Areado',
+    color: const [Color(0xFFFF3868), Color(0xFFFFB49A)],
+  ),
+  ExampleCandidateModel(
+    name: 'Two, 2',
+    job: 'Manager',
+    city: 'New York',
+    color: const [Color(0xFF736EFE), Color(0xFF62E4EC)],
+  ),
+  ExampleCandidateModel(
+    name: 'Three, 3',
+    job: 'Engineer',
+    city: 'London',
+    color: const [Color(0xFF2F80ED), Color(0xFF56CCF2)],
+  ),
+  ExampleCandidateModel(
+    name: 'Four, 4',
+    job: 'Designer',
+    city: 'Tokyo',
+    color: const [Color(0xFF0BA4E0), Color(0xFFA9E4BD)],
+  ),
+];
+
+class ExampleCard extends StatelessWidget {
+  final ExampleCandidateModel candidate;
+
+  const ExampleCard(
+    this.candidate, {
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      clipBehavior: Clip.hardEdge,
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(10)),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            spreadRadius: 3,
+            blurRadius: 7,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      alignment: Alignment.bottomRight,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Flexible(
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: candidate.color,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  candidate.name,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  candidate.job,
+                  style: const TextStyle(
+                    color: Colors.grey,
+                    fontSize: 15,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  candidate.city,
+                  style: const TextStyle(color: Colors.grey),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
 // try Turso for free persistant sql storage: https://docs.turso.tech/sdk/flutter/quickstart
 // put this in GitHub
@@ -12,7 +128,7 @@ import 'package:searchable_listview/searchable_listview.dart';
 
 late LibsqlClient client;
 
-void main()  {
+void main() {
   runApp(const MyApp());
 }
 
@@ -25,21 +141,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'My Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
         useMaterial3: true,
       ),
@@ -68,9 +169,25 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  List _myResult = [];
+  List _myRoomData = [
+    {
+      "id": '123',
+      'room_id': 'uty',
+      'room_name': 'fake room',
+      'task_id': 'jhg',
+      'task_name': 'fake task'
+    }
+  ];
 
-  Future<void> _incrementCounter()  async {
+  final CardSwiperController controller = CardSwiperController();
+  final cards = candidates.map(ExampleCard.new).toList();
+
+  @override
+  void dispose() {
+    controller.dispose();
+  }
+
+  Future<void> _incrementCounter() async {
     // make the connection near the point it is used so it doesn't time out
     await dotenv.load(fileName: ".env");
     String dbUrl = dotenv.env['TURSO_DATABASE_URL'] ?? '';
@@ -79,23 +196,22 @@ class _MyHomePageState extends State<MyHomePage> {
     log(dbToken);
     client = LibsqlClient(dbUrl, authToken: dbToken);
     await client.connect();
-    List result = await client.query("select name from rooms order by random();");
-    log(jsonEncode(result));
-    List _newResult = result;
+    List _newRoomData = await client.query(
+        "select room_tasks.*, room_name, task_name from room_tasks, rooms, tasks "
+            "where room_tasks.room_id=rooms.id and room_tasks.task_id=tasks.id "
+            "order by room_name, task_name;");
+    log(jsonEncode(_newRoomData));
 
     setState(() {
-
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
-      _myResult = _newResult;
-
+      _myRoomData = _newRoomData;
     });
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -105,11 +221,8 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    final List<String> entries = <String>['A', 'B', 'C'];
-    final List<int> colorCodes = <int>[600, 500, 100];
-    List<String> names = ['User 1', 'User 2', 'User 3', 'User 4'];
-    // String _stringResult = jsonEncode(_myResult);
-    log(jsonEncode(_myResult));
+
+    // log(jsonEncode(_myRoomData));
     String _stringResult = 'Hi Heather';
 
     return Scaffold(
@@ -142,25 +255,33 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              '$_stringResult\n\n You have pushed the button this many times:'
-            ),
+                '$_stringResult\n\n You have pushed the button this many times:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
             Expanded(
-              child:
-            ListView.builder(
-              padding: const EdgeInsets.all(8),
-              itemCount: _myResult.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Container(
-                  height: 50,
-                  // color: Colors.amber[colorCodes[index]],
-                  child: Center(child: Text(StringUtils.capitalize('${_myResult[index]["name"]}'))),
-                );
-              },
-            )
+                child: CardSwiper(
+                  controller: controller,
+                  cardsCount: cards.length,
+                  onSwipe: _onSwipe,
+                  onUndo: _onUndo,
+                  numberOfCardsDisplayed: 4,
+                  backCardOffset: const Offset(40, 40),
+                  padding: const EdgeInsets.all(24.0),
+                  cardBuilder: (context,
+                      index,
+                      horizontalThresholdPercentage,
+                      verticalThresholdPercentage,) =>
+                  cards[index],
+                )
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(15),
+                // child: simpleSearchWithSort(_myRoomData, controller),
+                child: simpleSearchWithSort(_myRoomData),
+              ),
             ),
           ],
         ),
@@ -172,4 +293,195 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+// Widget simpleSearchWithSort(List roomListofMapsData, CardSwiperController controller) {
+Widget simpleSearchWithSort(List roomListofMapsData) {
+  log("roomListofMapsData");
+  log(jsonEncode(roomListofMapsData));
+  List<RoomTask> roomDataAsRooms = [
+    for (Map RoomMap in roomListofMapsData) RoomTask.fromMap(RoomMap)
+  ];
+  // log("roomDataAsRooms");
+  // log(jsonEncode(roomDataAsRooms));
+
+  return SearchableList<RoomTask>(
+    lazyLoadingEnabled: false,
+    // sortWidget: Icon(Icons.sort),
+    // sortPredicate: (a, b) => a.full_name.compareTo(b.full_name),
+    filter: (p0) {
+      return roomDataAsRooms
+          .where((element) => element.full_name.contains(p0))
+          .toList();
+    },
+    itemBuilder: (item) {
+      // return RoomTaskItem(room_task: item, controller: controller);
+      return RoomTaskItem(room_task: item);
+    },
+    initialList: roomDataAsRooms,
+    inputDecoration: InputDecoration(
+      labelText: "Search Task",
+      fillColor: Colors.white,
+      focusedBorder: OutlineInputBorder(
+        borderSide: const BorderSide(
+          color: Colors.blue,
+          width: 1.0,
+        ),
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    ),
+  );
+}
+
+class EmptyView extends StatelessWidget {
+  const EmptyView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          Icons.error,
+          color: Colors.red,
+        ),
+        Text('no room is found with this room_name'),
+      ],
+    );
+  }
+}
+
+class RoomTask {
+  String id = 'abc';
+  String room_id = '234';
+  String room_name = 'Temp Room';
+  String task_id = 'sdf';
+  String task_name = 'Temp Task';
+  late String full_name = "$room_name $task_name";
+
+  RoomTask(
+      {required this.id,
+      required this.room_id,
+      required this.task_id,
+      required this.room_name,
+      required this.task_name});
+
+  RoomTask.fromMap(Map myMap) {
+    this.id = myMap["id"];
+    this.room_id = myMap["room_name"];
+    this.task_id = myMap["task_id"];
+    this.room_name = myMap["room_name"];
+    this.task_name = myMap["task_name"];
+  }
+}
+
+class RoomTaskItem extends StatelessWidget {
+  final RoomTask room_task;
+
+  // final CardSwiperController controller;
+  //
+  const RoomTaskItem({Key? key, required this.room_task
+      // ,required CardSwiperController this.controller
+      })
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      // child: GestureDetector(
+      //   onTap: () {
+      //     ScaffoldMessenger.of(context)
+      //         .showSnackBar(SnackBar(content: Text('Gesture Detected!')));
+      //   },
+      child: Container(
+        height: 60,
+        decoration: BoxDecoration(
+          color: Colors.grey[200],
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Card(
+          child: Row(
+            children: [
+              const SizedBox(
+                width: 10,
+              ),
+              Icon(
+                Icons.star,
+                color: Colors.yellow[700],
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // FloatingActionButton(
+                  //   onPressed: this.controller.undo,
+                  //   child: const Icon(Icons.rotate_left),
+                  // ),
+                  // FloatingActionButton(
+                  //   onPressed: () => this.controller.swipe(CardSwiperDirection.left),
+                  //   child: const Icon(Icons.keyboard_arrow_left),
+                  // ),
+                  // FloatingActionButton(
+                  //   onPressed: () =>
+                  //       this.controller.swipe(CardSwiperDirection.right),
+                  //   child: const Icon(Icons.keyboard_arrow_right),
+                  // ),
+                  // FloatingActionButton(
+                  //   onPressed: () => this.controller.swipe(CardSwiperDirection.top),
+                  //   child: const Icon(Icons.keyboard_arrow_up),
+                  // ),
+                  // FloatingActionButton(
+                  //   onPressed: () =>
+                  //       this.controller.swipe(CardSwiperDirection.bottom),
+                  //   child: const Icon(Icons.keyboard_arrow_down),
+                  // ),
+                  Text(
+                    'id: ${room_task.id}',
+                    style: const TextStyle(
+                      color: Colors.purple,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    '${room_task.full_name}',
+                    style: const TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+      // ),
+    );
+  }
+}
+
+bool _onSwipe(
+  int previousIndex,
+  int? currentIndex,
+  CardSwiperDirection direction,
+) {
+  debugPrint(
+    'The card $previousIndex was swiped to the ${direction.name}. Now the card $currentIndex is on top',
+  );
+  return true;
+}
+
+bool _onUndo(
+  int? previousIndex,
+  int currentIndex,
+  CardSwiperDirection direction,
+) {
+  debugPrint(
+    'The card $currentIndex was undod from the ${direction.name}',
+  );
+  return true;
 }
